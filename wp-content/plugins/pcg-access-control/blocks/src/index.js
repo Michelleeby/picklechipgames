ccd7aafdc97fa03ee119a676cfba0d4236d2cdd0
Michelle Byrnes <michelle.byrnes@automattic.com>
2025-05-11 04:36:56 -0400

    add utility shell scripts

--- wp-content/plugins/pcg-access-control/blocks/src/index.js
+++ wp-content/plugins/pcg-access-control/blocks/src/index.js
@@ -2,4 +2,4 @@
 import './register';
 
 // This file serves as the entry point for all blocks
-// Each block should be imported above 
+// Each block should be imported above
--- wp-content/plugins/pcg-campaign-core/blocks/src/index.js
+++ wp-content/plugins/pcg-campaign-core/blocks/src/index.js
@@ -2,4 +2,4 @@
 import './example-block';
 
 // This file serves as the entry point for all blocks
-// Each block should be imported above 
+// Each block should be imported above
